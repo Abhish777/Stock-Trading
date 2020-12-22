a131d5a51c8c909dbece714221d3ddf97a56af08
Abhish Ijari <ijariabhish@gmail.com>
2020-12-22 10:34:58 +0530

    Add files via upload

---
+++ StockPrice/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include " stock.h"
+
+q = queue_add(q,10, &res);
+q = queue_add(q,20, &res);
+assert(queue_length(q) == 3);
+assert(res.status == queue_full);
+
+
+
+void test_Qdelete(){
+Queue q1 = queue_new(3);
+Queue +q =  &q1;
+}
